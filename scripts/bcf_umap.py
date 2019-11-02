7f8f0e4f2cfe7d9bdb80d7ef2942025e18c01e2b
DomNelson <nelson.dominic@gmail.com>
2019-11-02 17:33:17 -0400

    First commit

---
+++ scripts/bcf_umap.py
@@ -0,0 +1,78 @@
+import os
+import numpy as np
+import umap
+import subprocess
+import argparse
+
+import sys
+sys.path.append(os.path.expanduser('~/project/msprime'))
+import msprime
+
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+from IPython import embed
+
+
+def get_prefix(args):
+    f = os.path.expanduser(args.bcf_file)
+    base, ext = os.path.splitext(f)
+    _, basename = os.path.split(base)
+
+    out_dir = os.path.expanduser(args.output_dir)
+    if not os.path.isabs(out_dir):
+        out_dir = os.path.abspath(out_dir)
+
+    prefix = os.path.join(os.path.abspath(out_dir), basename)
+
+    return prefix
+
+
+def main(args):
+    out_dir = os.path.expanduser(args.output_dir)
+    bcf_file = os.path.expanduser(args.bcf_file)
+    bcf_time = os.path.getmtime(bcf_file)
+    prefix = get_prefix(args)
+
+    evecs_file = prefix + '.eigenvec'
+    if (not os.path.exists(evecs_file) or
+            os.path.getmtime(evecs_file) < bcf_time):
+
+        print("Running PCA")
+        plink_pca_cmd = "plink --pca --bcf {} --memory 4000 --out {}".format(
+                bcf_file, prefix)
+        subprocess.run(plink_pca_cmd, shell=True, check=True)
+    else:
+        print("Using existing PCA file:", evecs_file)
+    pca_time = os.path.getmtime(bcf_file)
+
+    print("Running UMAP")
+    umap_file = prefix + '_umap.npy'
+    data = np.genfromtxt(evecs_file)
+    IDs, evecs = data[:, :2], data[:, 2:]
+
+    embedding = umap.UMAP(n_neighbors=5,
+                          min_dist=0.1).fit_transform(evecs)
+    np.save(umap_file, embedding)
+
+    plot_file = prefix + '_umap.png'
+    print("Plotting to", plot_file)
+    fig, ax = plt.subplots()
+    ax.scatter(embedding[:, 0], embedding[:, 1], s=1)
+    fig.savefig(plot_file)
+
+    if args.ipython:
+        embed()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-b', '--bcf-file', required=True)
+    parser.add_argument('-o', '--output-dir', required=True)
+    parser.add_argument('-d', '--ploidy', type=int, default=2)
+    parser.add_argument('-I', '--ipython', action='store_true')
+
+    args = parser.parse_args()
+
+    main(args)
---
+++ scripts/pipelines/pedigree_multi_chrom_umap.sh
@@ -0,0 +1,52 @@
+set -e
+
+N_CHROMS=4
+N_SAMPLES=100
+POPSIZE=100
+LEN=1e6
+MU=1e-8
+THREADS=2
+
+PEDFILE=~/project/pedigree_msp/data/pedEx.txt
+# PEDFILE=~/project/pedigree_msp/data/BALasc_probands1930.txt
+
+OUT_DIR=~/temp
+
+PREFIX=$OUT_DIR/${N_SAMPLES}_samples_${POPSIZE}_Ne_${LEN}_len_${MU}_mu
+
+cd ~/project/pedigree_msp/scripts
+CHROM_NUMS=$( seq 1 $N_CHROMS )
+
+for I in $CHROM_NUMS
+do
+    TS_FILES[$I]=${PREFIX}_chr$I.h5
+done
+
+## Build commands to run in parallel
+for I in $CHROM_NUMS
+do
+    F=${TS_FILES[$I]};
+    echo Simulating $F
+    CMDS[$I]="python pedigree_example.py "
+    CMDS[$I]+="-p $PEDFILE -o $F -n $N_SAMPLES -N $POPSIZE -m $MU -l $LEN;"
+    # CMDS[$I]="echo testing $I;"
+    # CMDS[$I]+=" sleep 1"
+done;
+
+## Simulate tree sequences
+for C in "${CMDS[@]}"
+do
+    echo Running $C
+done
+parallel -j $THREADS -- "${CMDS[@]}"
+
+## Combine to single BCF
+CMD="python ts_to_bcf.py -o ${PREFIX}_combined.bcf"
+for F in "${TS_FILES[@]}"
+do
+    CMD+=" -t $F"
+done
+eval $CMD -v
+
+## Run UMAP on combined BCF
+python bcf_umap.py --bcf-file ${PREFIX}_combined.bcf -o $OUT_DIR
