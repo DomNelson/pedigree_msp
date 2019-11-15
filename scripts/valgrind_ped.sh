61270ccc1ba22e935687c6be7f4f3354d0b6bf45
DomNelson <nelson.dominic@gmail.com>
2019-11-15 12:49:50 -0500

    Update valgrind test structure

--- scripts/valgrind_ped.sh
+++ scripts/valgrind_ped.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+# Get script directory regardless of where it was called from
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null 2>&1 && pwd)"
+
 cd ~/project/msprime/lib/build
 
-ninja && valgrind ./dev-cli simulate <(cat ~/project/pedigree_msp/data/example_for_ped.cfg ~/project/pedigree_msp/data/ll_msp_sample_config.txt ~/project/pedigree_msp/data/ll_pedEx.txt) -o ~/temp/trees.h5
+ninja && valgrind ./dev-cli simulate <(cat $SCRIPTDIR/example_for_ped.cfg $SCRIPTDIR/ll_msp_sample_config.txt $SCRIPTDIR/ll_pedEx.txt) -o ~/temp/trees.h5
 
